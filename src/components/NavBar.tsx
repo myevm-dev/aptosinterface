96e2a820051210830f3117489dc7f25cd1f924ec
My EVM <yo@myevm.org>
2024-10-08 22:31:14 +0000

    tkning

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,27 +11,28 @@ export function NavBar() {
   return (
     <nav className="navbar py-4 px-4">
       <div className="flex-1 text-lg font-semibold">
-        <Link href="/">
-          <div className="title cursor-pointer" style={{ fontSize: "24px" }}>Wolf Game</div>
+        <Link href="/" passHref>
+          <a className="title cursor-pointer" style={{ fontSize: "24px" }}>
+            {/* Replacing text with SVG Image */}
+            <Image
+              src="/thetoken.svg"   // Make sure the SVG is located in your /public directory
+              alt="Wolf Game Logo"
+              width={420}            // Set appropriate width for the image
+              height={80}            // Set appropriate height for the image
+            />
+          </a>
         </Link>
-        {NETWORK == "mainnet" ?
+        {NETWORK === "mainnet" ? (
           <div className="cursor-pointer ml-2 text-red title-upper" style={{ fontSize: "14px" }}>Aptos</div>
-          : <div className="cursor-pointer ml-2 text-red title-upper" style={{ fontSize: "14px" }}>{NETWORK}</div>}
+        ) : (
+          <div className="cursor-pointer ml-2 text-red title-upper" style={{ fontSize: "14px" }}>{NETWORK}</div>
+        )}
         <ul className="menu menu-horizontal p-0 ml-5">
           <li className="font-sans text-lg">
             <Link href='/game'>Play</Link>
           </li>
           <li className="font-sans text-lg">
             <Link href="/riskygame" target="_blank">Risky Game</Link>
-          </li>
-          <li className="font-sans text-lg">
-            <Link href="/whitepapers" target="_blank">Whitepapers</Link>
-          </li>
-          <li className="font-sans text-lg">
-            <Link href="https://test.wolfgameaptos.xyz" target="_blank">Testnet</Link>
-          </li>
-          <li className="font-sans text-lg">
-            <Link href="https://github.com/AptosWolfGame" target="_blank">Source Code</Link>
           </li>
         </ul>
       </div>
